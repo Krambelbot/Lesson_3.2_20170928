3c797d7f2196fa6c6cc20e4b8b80340bd1fb5167
Krambelbot <Mindwalk85@gmail.com>
2017-10-05 23:30:53 +0300

    add .gitignore and some others

---
+++ Lesson_3.2.py
@@ -0,0 +1,14 @@
+from collections import OrderedDict
+
+# another comment
+
+od = OrderedDict({
+    ('pen', 3),
+    ('pineapple', 2),
+    ('apple', 1)
+})
+
+
+
+print(od.values())
+print(od.keys())
