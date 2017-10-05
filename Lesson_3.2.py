4e63742fc736bd26ba815b8a79be9795b0ba5703
Krambelbot <Mindwalk85@gmail.com>
2017-10-05 23:39:22 +0300

    Ver 0.2.

--- Lesson_3.2.py
+++ Lesson_3.2.py
@@ -1,14 +1,22 @@
 from collections import OrderedDict
 
 # another comment
-
 od = OrderedDict({
     ('pen', 3),
     ('pineapple', 2),
     ('apple', 1)
 })
 
-
-
 print(od.values())
 print(od.keys())
+
+# version 0.2
+
+d = {
+    'pen': 3,
+    'pineapple': 2,
+    'apple': 1
+}
+
+print(d.values())
+print(d.keys())
